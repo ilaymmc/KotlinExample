3f74a500fec870974954f11aad9504d948764349
Ilya Manin <ilay@spbtv.com>
2020-07-21 13:51:38 +0300

    Initial commit

---
+++ app/src/main/java/ru/skillbranch/kotlinexample/UserHolder.kt
@@ -0,0 +1,25 @@
+package ru.skillbranch.kotlinexample
+
+object UserHolder {
+    private val map = mutableMapOf<String, User>()
+
+    fun registerUser(
+        fullName: String,
+        email: String,
+        password: String
+    ) : User {
+        return User.makeUser(fullName, email = email, password = password)
+            .also { user -> map[user.login] = user }
+    }
+
+    fun loginUser(login: String, password: String) : String? {
+        return map[login.trim()]?.run {
+            if (checkPassword(password)) this.userInfo
+            else null
+        }
+    }
+
+    fun importUsers(csv: List<String>) : List<User> {
+        return listOf<User>()
+    }
+}
